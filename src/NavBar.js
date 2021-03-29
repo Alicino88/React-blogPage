8efc6f8330bee9f5f30a9ecb239fd819bfc071d3
Alice <alice.moretti@truemedinc.com>
2021-03-29 13:12:20 +0300

    Created NavBar component

---
+++ src/NavBar.js
@@ -0,0 +1,25 @@
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <h1>The blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a
+          href="/create"
+          /*Inline styling with key-value pairs: 
+          inner brackets represent the js object and the outher the dynamic value
+          note. The value is always a string and minus sign cannot be used, instead camelcase*/
+          style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px",
+          }}
+        >
+          Write blog
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
