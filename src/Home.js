3a47da1743cd61ae74363f8d390f56bdeea4b01b
Alice <alice.moretti@truemedinc.com>
2021-03-29 14:05:22 +0300

    Added filter method to display only certain blogs

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,41 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
     {
       title: "Blog1",
       content: "Ciao Ciao ciao ciao ciao ciao",
-      author: "name",
+      author: "alice",
       id: 1,
     },
     {
       title: "Blog2",
       content: "Ciao Ciao ciao ciao ciao ciao",
-      author: "name",
+      author: "mario",
       id: 2,
     },
     {
       title: "Blog3",
       content: "Ciao Ciao ciao ciao ciao ciao",
-      author: "name",
+      author: "mario",
       id: 3,
     },
   ]);
 
   return (
     <div className="home">
+      {/*below we are outputting all the blogs*/}
       {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
+        <BlogList id={blog.id} title={blog.title} author={blog.author} />
       ))}
+      {/*below we are outputting only the blogs with the author Alice. Note that after filtering through the array,
+       you still need to appply the map method to display the blogs*/}
+      {blogs
+        .filter((blog) => blog.author === "alice")
+        .map((blog) => (
+          <BlogList id={blog.id} title={blog.title} author={blog.author} />
+        ))}
     </div>
   );
 };
