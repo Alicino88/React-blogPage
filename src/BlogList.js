fbaa6f9f2555163308e2d2083dfa8fa322bfe0d1
Alice <alice.moretti@truemedinc.com>
2021-03-30 14:06:37 +0300

    Added json server package and fetched data

--- src/BlogList.js
+++ src/BlogList.js
@@ -6,8 +6,8 @@ const BlogList = ({ id, title, author, handleDelete }) => {
         <p>Written by {author}</p>
         {/*we wrap the function inside an anonymous one so to be able to pass 
         an argument, otherwise the function would fire immediately
-        handleDelete is a function which is passed as a prop*/}
-        <button onClick={() => handleDelete(id)}>Delete blog </button>
+        handleDelete is a function which is passed as a prop
+        <button onClick={() => handleDelete(id)}>Delete blog </button>*/}
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,47 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "Blog1",
-      content: "Ciao Ciao ciao ciao ciao ciao",
-      author: "alice",
-      id: 1,
-    },
-    {
-      title: "Blog2",
-      content: "Ciao Ciao ciao ciao ciao ciao",
-      author: "mario",
-      id: 2,
-    },
-    {
-      title: "Blog3",
-      content: "Ciao Ciao ciao ciao ciao ciao",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   /*The function below deletes the blogs when clicking on the button
-  it doesn't change the original data but it returns a new array*/
+  it doesn't change the original data but it returns a new array
   const handleDelete = (id) => {
-    /*the new array includes all the blog posts whose id is different from the one of the post we have clicked on the delete button*/
+    the new array includes all the blog posts whose id is different from the one of the post we have clicked on the delete button
     const newBlogs = blogs.filter((blog) => blog.id !== id);
-    /*we set the state so that React triggers the re-render of the template displaying all the blog posts but the one which has been deleted*/
+    we set the state so that React triggers the re-render of the template displaying all the blog posts but the one which has been deleted
     setBlogs(newBlogs);
-  };
+  };*/
+
+  useEffect(() => {
+    fetch(" http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
   return (
     <div className="home">
-      {/*below we are outputting all the blogs*/}
-      {blogs.map((blog) => (
-        <BlogList
-          id={blog.id}
-          title={blog.title}
-          author={blog.author}
-          handleDelete={handleDelete}
-        />
-      ))}
+      {/*below we are outputting all the blogs
+      we need to use logical && as otherwise we would get an error saying that
+      blogs is null. At the very beginning blog is null so we need to wait until
+      the value is not null to map the array and render the blog posts.
+      How it works is that it checks what is on the left of the && and if
+      that is true then it will bother executing the rest of the code
+      null is equal to false so the map function wont run until the
+      actual blogs are fetched. */}
+      {blogs &&
+        blogs.map((blog) => (
+          <BlogList id={blog.id} title={blog.title} author={blog.author} />
+        ))}
     </div>
   );
 };
