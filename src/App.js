bad57dbf6da84da21abf83c8b6dcb77699d08321
Alice <alice.moretti@truemedinc.com>
2021-03-31 14:08:45 +0300

    Added react router inside components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import Navbar from "./NavBar";
 import Home from "./Home";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Create";
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          {/*the switch component is to make sure only one component renders at once*/}
+          <Switch>
+            {/*the exact prop is to make sure that a component is rendered only when
+            the path is exactly the same and not just part of it*/}
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route exact path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+/*when we use link instead of a classic a link, react router prevents
+from sending a request to the server and the browsers handles showing
+the right component according to the url path*/
 function NavBar() {
   return (
     <nav className="navbar">
       <h1>The blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a
-          href="/create"
+        <Link to="/">Home</Link>
+        <Link
+          to="/create"
           /*Inline styling with key-value pairs: 
           inner brackets represent the js object and the outher the dynamic value
           note. The value is always a string and minus sign cannot be used, instead camelcase*/
@@ -16,7 +20,7 @@ function NavBar() {
           }}
         >
           Write blog
-        </a>
+        </Link>
       </div>
     </nav>
   );
