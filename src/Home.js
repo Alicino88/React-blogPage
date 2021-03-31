962523a02937d2cd07cc5f04d2889729eabdead5
Alice <alice.moretti@truemedinc.com>
2021-03-31 10:28:11 +0300

    Added error message in case we cannot fetch blogs

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,8 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  /*below we store error message to show it in the browser*/
+  const [isError, setIsError] = useState(null);
 
   /*The function below deletes the blogs when clicking on the button
   it doesn't change the original data but it returns a new array
@@ -17,6 +19,12 @@ const Home = () => {
   useEffect(() => {
     fetch(" http://localhost:8000/blogs")
       .then((res) => {
+        /*below we detect errors in case we get a response which is empty
+          or has some other error*/
+        if (!res.ok) {
+          /*the error we throw here is catched below*/
+          throw Error("Could not fetch the data");
+        }
         return res.json();
       })
       .then((data) => {
@@ -25,6 +33,12 @@ const Home = () => {
         /*once the blogs are set as the current state, and therefore are being shown in the browser,
         we change isPending to false so that the loading message is not shown anymore*/
         setIsPending(false);
+        setIsError(null);
+      })
+      /*below will catch the network errors, for example if we cannot connect to the server*/
+      .catch((err) => {
+        setIsPending(false);
+        setIsError(err.message);
       });
   }, []);
   return (
@@ -38,6 +52,7 @@ const Home = () => {
       null is equal to false so the map function wont run until the
       actual blogs are fetched. */}
       {/*below we show a message while the blogs are being fetched*/}
+      {isError && <div>{isError}</div>}
       {isPending && <div>Loading posts...</div>}
       {blogs &&
         blogs.map((blog) => (
