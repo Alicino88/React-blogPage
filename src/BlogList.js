081901a61c7d10dbabfc45518fe0eefc7e5da3d7
Alice <alice.moretti@truemedinc.com>
2021-04-14 14:41:05 +0300

    Added prop object on BlogList component

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,13 +1,13 @@
-const BlogList = ({ id, title, author, handleDelete }) => {
+const BlogList = (props, handleDelete) => {
   return (
     <div className="blog-list">
-      <div className="blog-preview" key={id}>
-        <h2>{title}</h2>
-        <p>Written by {author}</p>
+      <div className="blog-preview" key={props.id}>
+        <h2>{props.title}</h2>
+        <p>Written by {props.author}</p>
         {/*we wrap the function inside an anonymous one so to be able to pass 
         an argument, otherwise the function would fire immediately
         handleDelete is a function which is passed as a prop
-        <button onClick={() => handleDelete(id)}>Delete blog </button>*/}
+        <button onClick={() => handleDelete(props.id)}>Delete blog </button>*/}
       </div>
     </div>
   );
