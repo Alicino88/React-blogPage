d1cc866766910317dcf5aa41528f2c7ec1f0009d
Alice <alice.moretti@truemedinc.com>
2021-04-16 10:47:09 +0300

    used  id params to fetch data

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,25 @@
 import { useParams } from "react-router";
+import useFetch from "./UseFetch";
 
 const BlogDetails = () => {
-  /*useParams allows to grab route parameters from the route*/
+  /*useParams allows to grab route parameters from the route
+  The name must be the same here in the hook and on the router (id) in the App component*/
   const { id } = useParams();
+  /* Below I fetch the data using the id parameter. Data are fetched only for the blog post I have clicked on*/
+  const { data: blog, isPending, isError } = useFetch(
+    " http://localhost:8000/blogs/" + id
+  );
   return (
     <div className="blog-details">
-      <h2>Blog details - {id}</h2>
+      {isPending && <div>Loading... </div>}
+      {isError && <div>{isError}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.content}</div>
+        </article>
+      )}
     </div>
   );
 };
